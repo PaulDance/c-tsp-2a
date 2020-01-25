3fe40afc9f74cfe425750c11f965d2a1e5efa714
Paul Mabileau <paulmabileau@hotmail.fr>
2020-01-25 16:32:28 +0100

    Renamed TP2
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- TP2-Arrays-Structures/sort.h
+++ TP2-Arrays-Structures/sort.h
@@ -4,12 +4,12 @@
 #include <glob.h>
 
 
-#ifndef TP2_ARRAYS_POINTERS_SORT_H
-#define TP2_ARRAYS_POINTERS_SORT_H
+#ifndef TP2_ARRAYS_STRUCTURES_SORT_H
+#define TP2_ARRAYS_STRUCTURES_SORT_H
 
 int getInsertIndex(float* array, int maxIndex, float value);
 void insertValueAt(float* array, int index, int maxIndex, float value);
 void insertValue(float* array, int maxIndex, float value);
 void sort(float* array, float* resultBuffer, size_t size);
 
-#endif //TP2_ARRAYS_POINTERS_SORT_H
+#endif //TP2_ARRAYS_STRUCTURES_SORT_H
