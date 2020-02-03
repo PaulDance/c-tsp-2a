ab923fdd9bfe373c00e6211d4f942a2b3a24bba4
Paul Mabileau <paulmabileau@hotmail.fr>
2020-02-03 19:51:23 +0100

    TP 4 - Ex 3
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

---
+++ TP4-Pointers/ex3-args.c
@@ -0,0 +1,17 @@
+//
+// Created by pauldance on 03/02/2020.
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char** argv) {
+	int sum = 0;
+	
+	for (int i = 0; i < argc; i++) {
+		printf("argv[%i] @ %p = %s\n", i, &argv[i], argv[i]);
+		sum += atoi(argv[i]);
+	}
+	
+	printf("Sum of arguments = %i\n", sum);
+}
