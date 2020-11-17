0a773d6e7c973cddbdb3a3f9308c2b8717239c05
Paul Mabileau <paulmabileau@hotmail.fr>
2020-11-17 20:32:21 +0100

    Reformat and refactor TP5
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- TP5-Files/cat-bank-file.c
+++ TP5-Files/cat-bank-file.c
@@ -6,17 +6,17 @@
 
 
 int main(int argc, char** argv) {
-	if (argc == 2) {
-		const int blockSize = 1024;
-		char readString[blockSize];
-		FILE* file = fopen(argv[1], "r");
-		
-		if (file != NULL) {
-			while (fgets(readString, blockSize, file)) {
-				printf("%s", readString);
-			}
-		}
-	}
-	
-	return EXIT_SUCCESS;
+    if (argc == 2) {
+        const int blockSize = 1024;
+        char readString[blockSize];
+        FILE* file = fopen(argv[1], "r");
+
+        if (file != NULL) {
+            while (fgets(readString, blockSize, file)) {
+                printf("%s", readString);
+            }
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
